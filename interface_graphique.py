179e855d65c30e7921e35b857a35bad47a1a369e
Anis Ch <anissdjahid.chellat@monboreal.ca>
2026-04-20 01:52:01 -0400

    Implémentation du cadre actions

--- interface_graphique.py
+++ interface_graphique.py
@@ -31,3 +31,19 @@ class ViewSalle(ctk.CTk):
         ctk.CTkLabel(self.f1, text="Capacité").grid(row=3, column=0)
         self.capacite = ctk.CTkEntry(self.f1)
         self.capacite.grid(row=3, column=1)
+
+
+        self.f2 = ctk.CTkFrame(self)
+        self.f2.pack(pady=10)
+
+        self.btn_ajouter = ctk.CTkButton(self.f2, text="Ajouter")
+        self.btn_ajouter.grid(row=0, column=0)
+
+        self.btn_modifier = ctk.CTkButton(self.f2, text="Modifier")
+        self.btn_modifier.grid(row=0, column=1)
+
+        self.btn_supprimer = ctk.CTkButton(self.f2, text="Supprimer")
+        self.btn_supprimer.grid(row=0, column=2)
+
+        self.btn_rechercher = ctk.CTkButton(self.f2, text="Rechercher")
+        self.btn_rechercher.grid(row=0, column=3)
